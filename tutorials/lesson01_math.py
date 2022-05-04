5853ff25ab2bd316de6824063df7be3987f33c8f
dfg-98 <dariofg98@gmail.com>
2022-05-04 17:44:55 -0400

    refactor: encapsulate importing patch in common script

--- tutorials/lesson01_math.py
+++ tutorials/lesson01_math.py
@@ -1,17 +1,4 @@
-# If file is run as a script add parent directory to path
-# This allow import rendering module
-if __name__ == "__main__":
-    import sys
-    import os
-    import inspect
-
-    currentdir = os.path.dirname(
-        os.path.abspath(inspect.getfile(inspect.currentframe()))
-    )
-    parentdir = os.path.dirname(currentdir)
-    sys.path.insert(0, parentdir)
-    ROOT_DIR = str(parentdir)
-
+import lesson_common
 
 # importing rendering functions.
 import rendering as ren
--- tutorials/lesson06_loading_obj.py
+++ tutorials/lesson06_loading_obj.py
@@ -1,18 +1,4 @@
-ROOT_DIR = "./"  # Used to find the models folder
-
-# If file is run as a script add parent directory to path
-# This allow import rendering module
-if __name__ == "__main__":
-    import sys
-    import os
-    import inspect
-
-    currentdir = os.path.dirname(
-        os.path.abspath(inspect.getfile(inspect.currentframe()))
-    )
-    parentdir = os.path.dirname(currentdir)
-    sys.path.insert(0, parentdir)
-    ROOT_DIR = str(parentdir)
+from lesson_common import ROOT_DIR
 
 import rendering as ren
 import time
